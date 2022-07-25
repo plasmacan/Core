3688ad3765a56e9a6b9d461d0025d0c5abea00e3
Chris <grintor@gmail.com>
2022-07-25 22:21:31 +0000

    new info

--- src/plasma/webapp1.py
+++ src/plasma/webapp1.py
@@ -1,21 +1,15 @@
 # import json
-import os
-import types
 
 
-def main(response, request):
-    # response.data = json.dumps(dict(request.headers))
+def main(response, request, json):
+    response.data = json.dumps(dict(request.headers))
+    response.data = "foobasf"
 
-    request.environ["apig_wsgi.context"] = types.SimpleNamespace()
-    request.environ["apig_wsgi.context"].get_remaining_time_in_millis = time_remaining
+    # response.data = str(request.environ["apig_wsgi.context"].get_remaining_time_in_millis())
+    # response.data = str(request.start_time)
+    # response.data = f"{request.url} 1"
+    # havve you seen this?
 
-    response.data = str(request.environ["apig_wsgi.context"].get_remaining_time_in_millis())
-    response.data = str(request.start_time)
-    response.data = os.environ["var1"]
-
-    response.headers["Cache-Control"] = "max-age=0"
+    response.headers["Cache-Control"] = "must-revalidate no-transform s-maxage=10 max-age=0"
+    # "Cache-Control: no-store"
     return response
-
-
-def time_remaining():
-    return 123
